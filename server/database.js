555569f82c22abfd3649f7fcb1da16a6ba2c5edc
H0OE <rodrigoquiroz308@gmail.com>
2022-01-13 18:18:14 -0400

    db

--- server/database.js
+++ server/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const URI = 'mongodb://localhost/shoplist' //TODO
+//const URI = 'mongodb://127.0.0.1/shoplist' //TODO
 
 mongoose.connect(URI)
 .then(db => console.log('DB connected'))
--- server/dist/database.dev.js
+++ server/dist/database.dev.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose');
 
 var URI = 'mongodb://localhost/shoplist'; //TODO
+//const URI = 'mongodb://127.0.0.1/shoplist' //TODO
 
 mongoose.connect(URI).then(function (db) {
   return console.log('DB connected');
