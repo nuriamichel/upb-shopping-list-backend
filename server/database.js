1d5118cfad591d8956641f2df47191e513adb1f6
H0OE <rodrigoquiroz308@gmail.com>
2022-01-13 20:57:49 -0400

    db

--- server/database.js
+++ server/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-const URI = 'mongodb://localhost/shoplist' //TODO
+//const URI = 'mongodb://localhost/shoplist' //TODO
 //const URI = 'mongodb://127.0.0.1/shoplist' //TODO
-
+const URI = 'mongodb://localhost:27017/shoplist?authSource=shoplist --username shoplist --password dti2022'
 mongoose.connect(URI)
 .then(db => console.log('DB connected'))
 .catch(err => console.error(err))
