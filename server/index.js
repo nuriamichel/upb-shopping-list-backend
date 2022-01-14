da152b33bf5d89e06598996c24158afc2edc5ade
H0OE <rodrigoquiroz308@gmail.com>
2022-01-14 13:17:30 -0400

    db

--- server/index.js
+++ server/index.js
@@ -9,13 +9,13 @@ const {mongoose} = require('./database')
 
 
 // Settings
-app.set('port', process.env.PORT || 27017)
+app.set('port', process.env.PORT || 3000)
 
 //Middlewares
 app.use(morgan('dev'))
 app.use(express.json())
 //app.use(cors({origin: 'http://localhost:4200'}))//TODO
-app.use(cors({origin: 'http://skynet.lp.upb.edu/~shoplist/'}))//TODO
+app.use(cors({origin: 'https://skynet.lp.upb.edu/~shoplist/'}))//TODO
 
 //Routes
 app.use('/',require('./routes/users.routes'))// '/otra ruta'
