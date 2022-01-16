b2245a98ceee64f1b20cff7af547edfa4c89c764
H0OE <rodrigoquiroz308@gmail.com>
2022-01-15 20:31:23 -0400

    test3

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.set('port', process.env.PORT || 4500)
 app.use(morgan('dev'))
 app.use(express.json())
 //app.use(cors({origin: 'http://localhost:4200'}))//TODO
-app.use(cors({origin: 'https://skynet.lp.upb.edu/~shoplist/'}))//TODO
+app.use(cors())//TODO
 
 //Routes
 app.use('/',require('./routes/users.routes'))// '/otra ruta'
