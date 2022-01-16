ce832fa05fcab2285bf95dbbdacdee39697c9991
H0OE <rodrigoquiroz308@gmail.com>
2022-01-15 20:38:39 -0400

    test3

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const {mongoose} = require('./database')
 app.set('port', process.env.PORT || 4500)
 
 //Middlewares
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 app.use(morgan('dev'))
 app.use(express.json())
 //app.use(cors({origin: 'http://localhost:4200'}))//TODO
