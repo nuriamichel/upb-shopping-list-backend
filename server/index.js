e0a6ae24de4398f930214da767303753484dcd39
H0OE <rodrigoquiroz308@gmail.com>
2022-01-15 22:07:18 -0400

    test3

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const {mongoose} = require('./database')
 
 
 // Settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 4500)
 
 //Middlewares
 
@@ -18,11 +18,12 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())//TODO
 //app.use(cors({origin: 'https://skynet.lp.upb.edu/~shoplist/'}))//TODO
+app.use(cors({origin: 'http://localhost:4500/'}))//TODO
 
 //Routes
 app.use('/~shoplist',require('./routes/users.routes'))// '/otra ruta'
 
 //Starting Server+
-app.listen(app.get('port'),'186.121.251.3', () => {
+app.listen(app.get('port'), () => {
  console.log("server OK " + app.get('port'));   
 })
