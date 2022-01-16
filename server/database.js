ca78ba84f493fd9ccf9118c28da642d368045f6d
H0OE <rodrigoquiroz308@gmail.com>
2022-01-15 20:22:56 -0400

    test

--- server/database.js
+++ server/database.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 const user = 'shoplist'
 const password = 'dti2022'
 
-const URI = 'mongodb://localhost/shoplist' //TODO
+//const URI = 'mongodb://localhost/shoplist' //TODO
 //const URI = 'mongodb://127.0.0.1:27017/?authSource=shoplist&gssapiServiceName=mongodb' //TODO
 //const URI = 'mongodb://localhost:27017/shoplist?authSource=shoplist --username shoplist --password dti2022'
-//const URI ='mongodb://shoplist:dti2022@localhost:27017/shoplist?authSource=shoplist&gssapiServiceName=mongodb'
+const URI ='mongodb://shoplist:dti2022@localhost:27017/shoplist'
 mongoose.connect(URI)
 .then(db => console.log('DB connected'))
 .catch(err => console.error(err))
