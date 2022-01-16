94e8fb1bada2ba0e07b33c19a7a0e1bbae092a26
H0OE <rodrigoquiroz308@gmail.com>
2022-01-15 21:01:06 -0400

    test3

--- server/index.js
+++ server/index.js
@@ -12,33 +12,15 @@ const {mongoose} = require('./database')
 app.set('port', process.env.PORT || 4500)
 
 //Middlewares
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://skynet.lp.upb.edu');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 
 app.use(morgan('dev'))
 app.use(express.json())
 //app.use(cors({origin: 'http://localhost:4200'}))//TODO
-//app.use(cors({origin: 'https://skynet.lp.upb.edu'}))//TODO
+app.use(cors({origin: 'https://skynet.lp.upb.edu'}))//TODO
 
 //Routes
-app.use('/',require('./routes/users.routes'))// '/otra ruta'
+app.use('https://skynet.lp.upb.edu/~shoplist',require('./routes/users.routes'))// '/otra ruta'
 
 //Starting Server+
 app.listen(app.get('port'), () => {
