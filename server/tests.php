b73a4fbc397d5c5205235fd5d83fdcff1ae7d109
H0OE <rodrigoquiroz308@gmail.com>
2022-01-17 11:20:00 -0400

    mongo

--- server/tests.php
+++ server/tests.php
@@ -1,20 +1,8 @@
 <?php
-   // PHP version 7.4 used here
-    try {
-        // connect to OVHcloud Public Cloud Databases for MongoDB (cluster in version 4.4, MongoDB PHP Extension in 1.8.1)
-        $m = new MongoDB\Driver\Manager('mongodb://shoplist:dti2022@localhost:27017/shoplist');
-        echo "Connection to database successfully";
-        // display the content of the driver, for diagnosis purpose
-        $insertOneResult = $m->insertOne([
-            'username' => 'admin',
-            'email' => 'admin@example.com',
-            'name' => 'Admin User',
-        ]);
-        printf("Inserted %d document(s)\n", $insertOneResult->getInsertedCount());
+require 'vendor/autoload.php'; // incluir lo bueno de Composer
+$cliente = new MongoDB\Client("mongodb://shoplist:dti2022@localhost:27017");
+$c = $cliente->shoplist->users;
+$resultado = $c->find();
+echo 'hola'
 
-    }
-    catch (Throwable $e) {
-        // catch throwables when the connection is not a success
-        echo "Captured Throwable for connection : " . $e->getMessage() . PHP_EOL;
-    }
 ?>
