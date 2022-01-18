2e4550e8e9a76ec69808aa271f278ffe1d328e54
H0OE <rodrigoquiroz308@gmail.com>
2022-01-18 16:13:02 -0400

    addUsers

--- server/addUser.php
+++ server/addUser.php
@@ -3,8 +3,8 @@ $jsonMascota = json_decode(file_get_contents("php://input"));
 if (!$jsonMascota) {
     exit("No hay datos");
 }
-$bd = include_once "bd.php";
-$sentencia = $bd->prepare("insert into mascotas(id, email, photo, firstname) values (?,?,?,?)");
+$bd = include_once "conect.php";
+$sentencia = $bd->prepare("INSERT INTO users(id, email, photo, firstname) VALUES (?,?,?,?)");
 $resultado = $sentencia->execute([$jsonMascota->id,$jsonMascota->email, $jsonMascota->photo, $jsonMascota->firstname]);
 echo json_encode([
     "resultado" => $resultado,
