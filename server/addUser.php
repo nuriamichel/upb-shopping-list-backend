0f2d363dd7ddd9e27fe07229a7717c9ac3929506
H0OE <rodrigoquiroz308@gmail.com>
2022-01-17 22:04:49 -0400

    addUser

--- server/addUser.php
+++ server/addUser.php
@@ -15,6 +15,9 @@ $jsonUser = json_decode(file_get_contents("php://input"));
 if (!$jsonUser) {
     exit("No hay datos");
 }
+echo json_encode([
+  "llego" => $jsonUser,
+]);
 $sentencia = $conn->prepare("insert into users(id, email, photo, firstname) values (?,?,?,?)");
 $resultado = $sentencia->execute([$jsonUser->_id, $jsonUser->email, $jsonUser->photo, $jsonUser->firstname]);
 echo json_encode([
