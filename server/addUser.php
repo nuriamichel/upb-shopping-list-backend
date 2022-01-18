7d9b22d3d0cd29a827ac5b1d1a9c7b7abf42c8c7
H0OE <rodrigoquiroz308@gmail.com>
2022-01-18 17:25:30 -0400

    addUsers

--- server/addUser.php
+++ server/addUser.php
@@ -21,17 +21,13 @@ $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
 
-//echo $request->{'id'};
+echo $request->{'email'};
 
 
-$sql = "INSERT INTO users (id, email, photo, firstname)
-VALUES (2, 'Final test', 'FinalTest', 'john@TEST.com')";
+$sql = "INSERT INTO users (id, email, photo, firstname) 
+VALUES (3,'sss', 'FinalTest', 'john@TEST.com')";
 
-if ($conn->query($sql) === TRUE) {
-  echo json_encode("New record created successfully");
-} else {
-  echo json_encode("Error: " . $sql . "<br>" . $conn->error);
-}
+
 
 $conn->close();
 
