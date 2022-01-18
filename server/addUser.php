2c0f787dc8a5a15fa554762110dc1ccee6a2642d
H0OE <rodrigoquiroz308@gmail.com>
2022-01-18 16:33:01 -0400

    addUser

--- server/addUser.php
+++ server/addUser.php
@@ -3,16 +3,10 @@
 $bd = include_once "conect.php";
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
-if(isset($postdata) && !empty($postdata))
-{
-  $sql = "INSERT INTO users(id,email,photo,firstname) VALUES ('{$request->id}','{$request->email}','{$request->photo}','{$request->firstname}')";
- // echo $sql;
-if ($mysqli->query($sql) === TRUE) {
+
  
  
-    echo json_encode("todo" => "OK",);
  
-}
-}
+
 ?>
 ?>
