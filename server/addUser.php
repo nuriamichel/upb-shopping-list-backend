cf4fc0b44342891a3bb504707437ad0904faa31b
H0OE <rodrigoquiroz308@gmail.com>
2022-01-17 22:16:48 -0400

    addUser

--- server/addUser.php
+++ server/addUser.php
@@ -15,12 +15,13 @@ $jsonUser = json_decode(file_get_contents("php://input"));
 if (!$jsonUser) {
     exit("No hay datos");
 }
+$sql = "INSERT INTO users (id, email, photo, firstname) 
+VALUES ('".$jsonUser->_id."','".$jsonUser->email. "','".$jsonUser->photo. "','".$jsonUser->firstname."')";
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-$sentencia = $conn->prepare("insert into users(id, email, photo, firstname) values (?,?,?,?)");
-$resultado = $sentencia->execute([$jsonUser->_id, $jsonUser->email, $jsonUser->photo, $jsonUser->firstname]);
-echo json_encode([
-    "resultado" => $resultado,
-    "llego" => $jsonUser->_id,
-
-]);
+$conn->close();
 ?>
