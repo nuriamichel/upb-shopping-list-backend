af0a07c2edc061be469495f895936cb90dd75628
H0OE <rodrigoquiroz308@gmail.com>
2022-01-18 16:22:44 -0400

    addUser

--- server/addUser.php
+++ server/addUser.php
@@ -1,12 +1,28 @@
 <?php
-$jsonMascota = json_decode(file_get_contents("php://input"));
-if (!$jsonMascota) {
-    exit("No hay datos");
-}
+
 $bd = include_once "conect.php";
-$sentencia = $bd->prepare("INSERT INTO users(id, email, photo, firstname) VALUES (?,?,?,?)");
-$resultado = $sentencia->execute([$jsonMascota->id,$jsonMascota->email, $jsonMascota->photo, $jsonMascota->firstname]);
-echo json_encode([
-    "resultado" => $resultado,
-]);
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+if(isset($postdata) && !empty($postdata))
+{
+  $id = mysqli_real_escape_string($mysqli, (int)($request->id));
+  $email = mysqli_real_escape_string($mysqli, (int)$request->email);
+   $photo = mysqli_real_escape_string($mysqli, trim($request->photo));
+  $firstname = mysqli_real_escape_string($mysqli, (int)$request->firstname);
+  $sql = "INSERT INTO users(id,email,photo,firstname) VALUES ('{$id}','{$email}','{$photo}','{$firstname}')";
+ // echo $sql;
+if ($mysqli->query($sql) === TRUE) {
+ 
+ 
+    $authdata = [
+      'id' => $id,
+	  'email' => $email,
+	  'photo' => $photo,
+      'firstname' => $firstname,
+    ];
+    echo json_encode($authdata);
+ 
+}
+}
+?>
 ?>
