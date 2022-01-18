6ef2d402af5434256f1cda9703640bc87977dd7a
H0OE <rodrigoquiroz308@gmail.com>
2022-01-18 17:17:55 -0400

    addUser

--- server/addUser.php
+++ server/addUser.php
@@ -21,7 +21,7 @@ $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
 
-echo $request->{'id'};
+//echo $request->{'id'};
 
 
 $sql = "INSERT INTO MyGuests (firstname, lastname, email)
