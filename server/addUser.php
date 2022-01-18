c4d29944544f3169200133a894b2cbfebb812c65
H0OE <rodrigoquiroz308@gmail.com>
2022-01-18 17:01:06 -0400

    addUser

--- server/addUser.php
+++ server/addUser.php
@@ -4,9 +4,15 @@ $bd = include_once "conect.php";
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
- 
- 
- 
 
-?>
+echo $request->{'id'};
+
+$sql = "INSERT INTO `users`(`id`,`email`,`photo`,`firstname`) VALUES (2,'test', 'test', 'jtest.com')";
+if ($bd->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $bd->error;
+}
+
+
 ?>
