33639b80ac93b06e9857d41fd152af4a8f1738fa
H0OE <rodrigoquiroz308@gmail.com>
2022-01-18 17:13:05 -0400

    addUser

--- server/addUser.php
+++ server/addUser.php
@@ -1,13 +1,29 @@
 <?php
 
-$bd = include_once "conect.php";
+//$bd = include_once "conect.php";
+
+
+//$sql = "INSERT INTO users (id,email,photo,firstname) VALUES (2,'test', 'test', 'jtest.com')";
+$servername = "localhost";
+$username = "shoplist";
+$password = "Shopping123HELP";
+$dbname = "shoplist";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
 
 echo $request->{'id'};
 
-$sql = "INSERT INTO users (id,email,photo,firstname) VALUES (2,'test', 'test', 'jtest.com')";
+
 $sql = "INSERT INTO MyGuests (firstname, lastname, email)
 VALUES ('Final test', 'FinalTest', 'john@TEST.com')";
 
@@ -17,5 +33,6 @@ if ($conn->query($sql) === TRUE) {
   echo json_encode("Error: " . $sql . "<br>" . $conn->error);
 }
 
+$conn->close();
 
 ?>
