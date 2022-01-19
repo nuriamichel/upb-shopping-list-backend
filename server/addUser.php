a664884d78ad63d8a666e6f22a70947d56fcddd2
H0OE <rodrigoquiroz308@gmail.com>
2022-01-18 22:22:49 -0400

    ListDefault

--- server/addUser.php
+++ server/addUser.php
@@ -31,7 +31,7 @@ $sql = "INSERT INTO users ( email, photo, firstname)
 VALUES ('$email', '$photo', '$firstname')";
 
 if ($conn->query($sql) === TRUE) {
-  echo json_encode(1);
+  echo json_encode(TRUE);
 } else {
   echo json_encode("Error: " . $sql . "<br>" . $conn->error);
 }
