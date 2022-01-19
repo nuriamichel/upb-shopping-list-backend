6554c02b3237f5b317814cff0e91e8ff3a61f45c
H0OE <rodrigoquiroz308@gmail.com>
2022-01-19 19:18:04 -0400

    addProducts

---
+++ server/addProduct.php
@@ -0,0 +1,41 @@
+<?php
+
+//$bd = include_once "conect.php";
+
+
+//$sql = "INSERT INTO users (id,email,photo,firstname) VALUES (2,'test', 'test', 'jtest.com')";
+$servername = "localhost";
+$username = "shoplist";
+$password = "Shopping123HELP";
+$dbname = "shoplist";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+
+$listaid = $request->{'listaid'};
+$producto = $request->{'producto'};
+
+$sql = "INSERT INTO productos ( lista_id, producto)
+VALUES ('$listaid', '$producto')";
+
+if ($conn->query($sql) === TRUE) {
+  echo json_encode("New record created successfully");
+} else {
+  echo json_encode("Error: " . $sql . "<br>" . $conn->error);
+}
+
+
+
+
+
+$conn->close();
+
+?>
