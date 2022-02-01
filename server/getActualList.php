7c0bbe94ee215683212ae46a8e39db46f272e021
H0OE <rodrigoquiroz308@gmail.com>
2022-02-01 06:30:46 -0400

    getList

---
+++ server/getActualList.php
@@ -0,0 +1,37 @@
+<?php
+
+//$bd = include_once "conect.php";
+
+
+//$sql = "INSERT INTO users (id,email,photo,firstname) VALUES (2,'test', 'test', 'jtest.com')";
+$servername = "localhost";
+$username = "shoplist";
+$password = "Shopping123HELP";
+$dbname = "shoplist";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+
+$email = $_GET['email'];
+$lista = $_GET['lista'];
+
+$sqllist = "SELECT id FROM listas WHERE email = '$email' and lista = '$lista'"; 
+$resultlist = $conn->query($sqllist);
+
+while($row = $resultlist->fetch_assoc()){
+  $jsonlist = $row;
+}
+
+
+
+echo json_encode($jsonlist);
+
+$conn->close();
+
+?>
