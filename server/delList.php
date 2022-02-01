bfd8e4e2a3d5fe41842238e1638aae8268e7614f
H0OE <rodrigoquiroz308@gmail.com>
2022-02-01 13:31:19 -0400

    ddd

---
+++ server/delList.php
@@ -0,0 +1,30 @@
+<?php
+
+//$bd = include_once "conect.php";
+
+
+//$sql = "INSERT INTO users (id,email,photo,firstname) VALUES (2,'test', 'test', 'jtest.com')";
+$servername = "localhost";
+$username = "shoplist";
+$password = "Shopping123HELP";
+$dbname = "shoplist";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$id = $_GET['lisid'];
+
+// sql to delete a record
+$sql = "DELETE FROM listas WHERE id='$id'";
+
+if ($conn->query($sql) === TRUE) {
+} else {
+}
+
+$conn->close();
+
+?>
