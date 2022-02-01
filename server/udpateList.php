6308478dccf561d9511e3711257431beb5f18522
H0OE <rodrigoquiroz308@gmail.com>
2022-02-01 13:05:39 -0400

    updatelist

---
+++ server/udpateList.php
@@ -0,0 +1,30 @@
+<?php
+
+//$bd = include_once "conect.php";
+
+
+//$sql = "INSERT INTO users (id,email,photo,firstname) VALUES (2,'test', 'test', 'jtest.com')";
+$servername = "localhost";
+$username = "shoplist";
+$password = "Shopping123HELP";
+$dbname = "shoplist";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$id = $_GET['lisid'];
+$name = $_GET['name'];
+
+$sql = "UPDATE listas SET lista='$name' WHERE id='$id'";
+
+if ($conn->query($sql) === TRUE) {
+} else {
+}
+
+$conn->close();
+
+?>
